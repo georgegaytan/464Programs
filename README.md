7b2187ac1cb65b888e1c855d3421a3b357c06bbf
m00sedog00 <m00sedog00@github.tamu.edu>
2016-10-31 12:56:06 -0500

    removed hw5p2txt, updated readme w/ howto, init p3cpp

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 464Programs
 extra/nonextra credit programs for 464
+
+# How to run
+## p2
+g++ p2.cpp -o p2
+./p2
+
+## p3
+g++ p3.cpp -o p3
+./p3
+
---
+++ p3.cpp
@@ -0,0 +1,13 @@
+//FOR THE USE IN CSCE464 
+//Copyright © 2016 by George Steven Gaytan
+//All Rights Reserved
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+}
