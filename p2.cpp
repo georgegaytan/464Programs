6012e97ec0cb1a40fef05440398f1275d29d0a9a
m00sedog00 <m00sedog00@github.tamu.edu>
2016-10-31 12:53:08 -0500

    no file, only cout. fixed unsynced count/Xi. output #success/failed

--- p2.cpp
+++ p2.cpp
@@ -4,41 +4,41 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <stdlib>
+#include <stdlib.h>
 
-int count = 0;
+using namespace std;
+
+int count = 1;
 int xCount = 0;
 const int packet_trans = 1000;
 
-class Simulation {
-    int X, Xi, Y, Ri;
-	bool success;
-}
+void p2();
 
-Simulation::Simulation(){
-	X = 3;
-	Y = 2;
-	Ri = 1;
-	Xi = 1;
-}
+struct Simulation {
+    int X = 3;
+    int Xi = 1;
+    int Ri = 1;
+	bool success;
+};
 
 int main(){
-	ofstream myfile;
-	myfile.open ("hw5p2.txt");
-	myfile << p2();
-	myfile.close();
+	p2();
+	// ofstream myfile;
+	// myfile.open ("hw5p2.txt");
+	// //myfile << p2();
+	// myfile.close();
 	return 0;
 
 }
 
-string p2(){
+void p2(){
 	Simulation sim;
 	vector<Simulation> simVect;
-	while (count < packet_trans){
+	while (count <= packet_trans){
 		xCount = 0;
 
 		while(xCount < sim.X){
-			if(sim.Xi >= 0 && sim.Xi <= 15){
+			if(0 <= sim.Xi <= 14){
 				if(sim.Ri == 1){
 					sim.Xi++;
 					sim.success = true;
@@ -59,7 +59,7 @@ string p2(){
 					simVect.push_back(sim);
 					count++;
 
-					if(sim.Xi == 16){
+					if(sim.Xi == 15){
 						break;
 					}
 
@@ -75,7 +75,10 @@ string p2(){
 				
 			}
 
-			if(sim.Xi >= 0 && sim.Xi <= 15){
+			if(15 <= sim.Xi <= 24){
+				//quick check to reinit loop if just exited previous if
+				if(xCount == sim.X) { break; }
+
                 if(sim.Ri == 1){
                     sim.Xi++;
                     sim.success = true;
@@ -92,11 +95,18 @@ string p2(){
                 }
                 if(sim.Ri == 3){
                     sim.Xi++;
+                    sim.success = true;
+                    simVect.push_back(sim);
+                    count++;
+                    xCount++;
+                }
+                if(sim.Ri == 4){
+                    sim.Xi++;
                     sim.success = false;
                     simVect.push_back(sim);
                     count++;
-
-                    if(sim.Xi == 16){
+					
+                    if(sim.Xi == 26){
                         break;
                     }
 
@@ -104,15 +114,127 @@ string p2(){
                     sim.success = false;
                     simVect.push_back(sim);
                     count++;
-
+					
                     sim.X++;
                     xCount == sim.X;
                     sim.Ri -= 2;
+				}
+			
+			}
+
+			if(25 <= sim.Xi <= 29){
+				//quick check to reinit loop if just exited previous if
+				if(xCount == sim.X) { break; }
+
+				if(sim.Ri == 1){
+					sim.Xi++;
+					sim.success = true;
+					simVect.push_back(sim);
+					count++;
+					xCount++;
+	            }
+				if(sim.Ri == 2){
+					sim.Xi++;
+					sim.success = true;
+					simVect.push_back(sim);
+					count++;
+					xCount++;
+				}
+				if(sim.Ri == 3){
+					sim.Xi++;
+					sim.success = false;
+					simVect.push_back(sim);
+					count++;
+
+					if(sim.Xi == 31){
+						break;
+					}
+
+                    sim.Xi++;
+                    sim.success = false;
+                    simVect.push_back(sim);
+                    count++;
+
+					sim.X++;
+					xCount == sim.X;
+					sim.Ri -= 2;
+				}
+				
+			}
+
+			if(sim.Xi >= 30){
+				//quick check to reinit loop if just exited previous if
+				if(xCount == sim.X) { break; }
+				//quick check for last transmission range
+				//this exits program upon 1k trans
+                if(sim.Xi >= 1000){
+                  	break;
+                }
+				// cout << "c " << count << "\n";
+				// cout << "Xi " << sim.Xi << "\n";
+
+				if(count == packet_trans) { break; }
+
+                if(sim.Ri == 1){
+                    sim.Xi++;
+                    sim.success = true;
+                    simVect.push_back(sim);
+                    count++;
+                    xCount++;
+                }
+                if(sim.Ri == 2){
+                    sim.Xi++;
+                    sim.success = true;
+                    simVect.push_back(sim);
+                    count++;
+                    xCount++;
+                }
+                if(sim.Ri == 3){
+                    sim.Xi++;
+                    sim.success = true;
+                    simVect.push_back(sim);
+                    count++;
+                    xCount++;
+                }
+                if(sim.Ri == 4){
+                    sim.Xi++;
+                    sim.success = false;
+                    simVect.push_back(sim);
+                    count++;
+					
+                    if(sim.Xi >= 1000){
+                        break;
+                    }
+
+                    sim.Xi++;
+                    sim.success = false;
+                    simVect.push_back(sim);
+                    count++;
+					
+                    sim.X++;
+                    xCount == sim.X;
+                    sim.Ri -= 2;
+				}
+			
+			}
 
 		}
-		
 		sim.Ri++;
 	}
+
+	int successes = 0;
+	int failures = 0;
+
+	for(int i = 0; i < 1000; i++){
+		cout << boolalpha;
+		cout << "Packet" << (i+1) << ":" << simVect[i].success << "\n";
+		
+		if (simVect[i].success) { successes++; }
+		if (!simVect[i].success) { failures++; }
+	}
+
+	cout << "We saw " << successes << "successful transmissions\n";
+	cout << "We saw " << failures << "failed transmissions\n";
 }
 
 
