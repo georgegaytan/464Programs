63622e6ab8e99fbc4d9554bfc6d731c7a0f2c342
m00sedog00 <m00sedog00@github.tamu.edu>
2016-11-03 16:10:33 -0500

    optimized approach. Fully implemented and tidied up the meat of the alg

--- p3.cpp
+++ p3.cpp
@@ -4,47 +4,75 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <math.h>
 #include <stdlib.h>
 
 using namespace std;
 
 //global variables
 const long NOISE = pow(10, -10);//10^-10 mW
-const long Bi = 25;//25 dB
-vector<char> nodes;//fill with nodes
+const long Bi = 316;//25 dB
 
 //object for Path_gains
-class Path_gains(){
+struct Path_gain{
 	char trans;//transmitter
 	char rec;//receiver
 	long power;//pathgain of transmission
 };
 
 //funct declarations
-void p3(int, vector<Path_gains>, vector<long>);
+void p3(int);
 //HELPERS
-vector<Path_gains> init_path_gains(vector<long> gains);
-vector<long> init_trans_pow(vector<long> init_pow);
+vector<Path_gain> init_path_gains();
 
 int main(){
 	//for mimicking hw problem
 	//set parameters here
 	int times = 4;
-	vector<long> gains; vector<long> init_pow;
-	gains.push
-
-	p3(times, init_path_gains(), init_trans_pow());
+	p3(times);
 }
 
-void p3(int times, vector<Path_gains> path_gains, 
-	vector<long> trans_pow){
-	
+void p3(int times){
+	vector<Path_gain> pgv = init_path_gains();
+	vector<double> trans_pow;
+	int count = 0;
+	for(int t = 1; t <= times; ++t){
+		if (count == 0){
+			trans_pow.push_back(Bi*(pgv[0].power + NOISE)/(pgv[2].power));
+			count++;
+			cout << "Power for A at time " << t << ":" << trans_pow[0] << endl;
+			trans_pow.push_back(Bi*(pgv[1].power + NOISE)/(pgv[3].power));
+			count++;
+			cout << "Power for C at time " << t << ":" << trans_pow[1] << endl;
+		} else {
+			trans_pow.push_back(Bi*trans_pow[count-1]*(pgv[0].power + NOISE)/(pgv[2].power));
+			cout << "Power for A at time " << t << ":" << trans_pow[count] << endl;
+			count++;
+			trans_pow.push_back(Bi*trans_pow[count-3]*(pgv[0].power + NOISE)/(pgv[3].power));
+			cout << "Power for B at time " << t << ":" << trans_pow[count] << endl;
+			count++;
+		}
+		
+	}
 }
 
-vector<Path_gains> init_path_gains(vector<long> gains){
-	
-}
+vector<Path_gain> init_path_gains(){
+	vector<Path_gain> pgv;//AB AD CB CD
+	Path_gain pg;
+	pg.trans = 'A';
+	pg.rec = 'B';
+	pg.power = pow(10,-5);
+	pgv.push_back(pg);
+	pg.rec = 'D';
+	pg.power = pow(10, -8.2);
+	pgv.push_back(pg);
+	pg.trans = 'C';
+	pg.rec = 'B';
+	pg.power = pow(10,-7.8);
+	pgv.push_back(pg);
+	pg.rec = 'D';
+	pg.power = pow(10, -6);
+	pgv.push_back(pg);
 
-vector<long> init_trans_pow(vector<long> init_pow){
-	
+	return pgv;
 }
