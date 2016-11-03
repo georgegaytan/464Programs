baf177bc21e37a7af962803db4dc9c4488567e94
m00sedog00 <m00sedog00@github.tamu.edu>
2016-11-03 00:43:44 -0500

    working on it

--- p3.cpp
+++ p3.cpp
@@ -8,6 +8,27 @@
 
 using namespace std;
 
-int main(){
+const long NOISE = pow(10, -10);//10^-10 mW
+const long Bi = 25;//25 dB
 
+
+class Path_gains(){
+	char trans;//transmitter
+	char rec;//receiver
+	long power;//pathgain of transmission
+};
+
+p3(int, vector<Path_gains>, vector<long>, 
+
+int main(){
+	//for mimicking hw problem
+	//set parameters here
+	int times = 4;
+
+	p3(times, init_path_gains(), init_trans_pow());
+}
+
+p3(int times, vector<pathgains> path_gains, 
+	vector<long> trans_pow, ){
+	
 }
